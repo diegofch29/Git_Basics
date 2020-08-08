d3a4f76ac11b5bd4a4f06566479528bf4a058acc
diegofch29 <diegofch29@hotmail.com>
2020-08-07 22:47:24 -0500

    Creacion de nuevos archivos Solucion Fin del archivo

---
+++ Final_Archivo/src/final_archivo/Final_Archivo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package final_archivo;
+
+/**
+ *
+ * @author diego
+ */
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Final_Archivo {
+
+    public static void main(String[] args) {
+        Scanner a=new Scanner(System.in);
+        int i=0;
+        while (a.hasNext()){
+            i+=1;
+            System.out.println(i+" "+a.nextLine());
+        }
+    }
+}
